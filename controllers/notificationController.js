9dea1cfcdc57f1ef034e9572d7884012f236eb00
MohammadEImmam <immam.m@ufl.edu>
2022-01-08 22:30:25 -0500

    notification??
    idk what

---
+++ controllers/notificationController.js
@@ -0,0 +1,33 @@
+export function GetNotification(){
+    const notifications = [{
+            id: "20331",
+            createdAt:"10-10-2020T6:15PM",
+            seen: false,
+            message: "Jagoron onek gay",
+            typeId: 1,
+            subjectId : 0
+        },{
+            id: "22222",
+            createdAt:"10-10-2020T6:15PM",
+            seen: false,
+            message: "Jagoron onek onek beshi gay",
+            typeId: 1,
+            subjectId : 0
+        },{
+            id: "33333",
+            createdAt:"10-10-2020T6:15PM",
+            seen: false,
+            message: "Jagoron onek onek gay",
+            typeId: 1,
+            subjectId : 0
+        },{
+            id: "4444",
+            createdAt:"10-10-2020T6:15PM",
+            seen: false,
+            message: "Jagoron onek beshi gay",
+            typeId: 1,
+            subjectId : 0
+        },
+    ]
+    return notifications
+}
