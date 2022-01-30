e1393e3f54ca7c66023995eba8b76a342966142f
MohammadEImmam <immam.m@ufl.edu>
2022-01-29 21:47:18 -0500

    emortion stuff done

--- controllers/EmortionController.js
+++ controllers/EmortionController.js
@@ -6,10 +6,36 @@ export function CreateEmortion(req, res){
     let _emortion = req.body;
     let newEmortion = new EmortionEngine(_emortion);
     newEmortion.save((err, addedEmortion)=>{
-        if(err)
+        if(err){
             console.log("Could not save to mongo!: "+err)
             res.send(err);
-        res.send(addedEmortion);
-        console.log("Emortion saved to mongo!");
+        }
+        else{
+            res.send(addedEmortion);
+            console.log("Emortion saved to mongo!");
+        }
+    })
+}
+
+export function GetEmortion(req,res){
+    EmortionEngine.findById({_id: req.params.id},
+        (err,emortion)=>{
+        if(err)
+            res.send(err)
+        res.send(emortion);
+    })
+}
+
+export function GetUserEmortions(req, res){
+    EmortionEngine.find({createdBy: req.params.id},
+        (err,emortion)=>{
+        if(err){
+            console.log(err)
+            res.send(err)
+        }
+        else{
+            console.log("Emortions found!")
+            res.send(emortion)
+        }
     })
 }
--- routes/EmortionRoutes.js
+++ routes/EmortionRoutes.js
@@ -1,6 +1,10 @@
-import{CreateEmortion} from "../controllers/EmortionController.js";
+import{CreateEmortion, GetEmortion, GetUserEmortions} from "../controllers/EmortionController.js";
 
 export default function EmortionRoutes (server) {
     server.route('/emortions/emortion')
         .post(CreateEmortion);
+    server.route('/emortions/emortion/:id')
+        .get(GetEmortion);
+    server.route('/user/emortionsById/:id')
+        .get(GetUserEmortions);
 }
