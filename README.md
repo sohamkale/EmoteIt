f7632cd4aadb480598bb3e93dcc92850bf247135
Mohammad A Immam <44204526+mimmam1464@users.noreply.github.com>
2022-05-15 22:08:59 -0400

    begin

--- README.md
+++
@@ -1,3 +0,0 @@
-# EmoteIt-Backend
-
-FrontEnd makes -> API call -> Routes -> Controller -> Mongo -> Controller (works on data) -> Routes -> API response -> FrontEnd
--- routes/LeaderboardRoutes.js
+++
@@ -1,7 +0,0 @@
-import {GetStats, TopThree} from "../controllers/LeaderboardController.js";
-export default function LeaderboardRoutes(server){
-    server.route('/api/leaderboard/stats')
-        .get(GetStats)
-    server.route('/api/leaderboard/top3')
-        .get(TopThree)
-}
